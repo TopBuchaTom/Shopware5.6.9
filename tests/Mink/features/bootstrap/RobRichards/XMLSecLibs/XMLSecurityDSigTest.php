fd859e2b18125a6ed537422d9b4ac702338bad4f
tzacher <thomas.zacher@th-deg.de>
2024-07-16 14:35:37 +0200

    uziuzfdgfdgfg

---
+++ tests/Mink/features/bootstrap/RobRichards/XMLSecLibs/XMLSecurityDSigTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Shopware\Tests\Mink\RobRichards\XMLSecLibs;
+
+use RobRichards\XMLSecLibs\XMLSecurityDSig;
+use PHPUnit\Framework\TestCase;
+
+class XMLSecurityDSigTest extends TestCase
+{
+
+}
